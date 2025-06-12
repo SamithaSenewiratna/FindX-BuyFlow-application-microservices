4ade9c39863c7b31df3710c9d7acc1f4dedfe302
samitha senewirathna <samithani17@gmail.com>
2025-06-13 00:29:31 +0530

    implement security-header-ui

--- E-commerce-front-End/cart-client/src/app/modules/process/process-routing.module.ts
+++ E-commerce-front-End/cart-client/src/app/modules/process/process-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContextComponent } from './comportents/context/context.component';
-import { HomepageComponent } from './comportents/homepage/homepage.component';
 
 
 const routes: Routes = [
-   { path: '', redirectTo: 'context', pathMatch: 'full' },
-  {
-    path: 'context',
-    component: ContextComponent,
-    children: [
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomepageComponent },
-    ],
-  },
+
 ];
 
 @NgModule({
