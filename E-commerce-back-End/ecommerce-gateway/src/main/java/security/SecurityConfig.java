b4fc10e0fe4bf1b08624a071d22b0a390b310fb8
samitha senewirathna <samithani17@gmail.com>
2025-07-07 14:51:52 +0530

    implemented gateway

--- E-commerce-back-End/ecommerce-gateway/src/main/java/security/SecurityConfig.java
+++ E-commerce-back-End/ecommerce-gateway/src/main/java/security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +23,17 @@ public class SecurityConfig {
                                 .authenticated())
                                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
                         return serverHttpSecurity.build();
+    }
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*")); //or use specific origins
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowCredentials(false);
+        org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource source =
+                new org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
     }
 }
