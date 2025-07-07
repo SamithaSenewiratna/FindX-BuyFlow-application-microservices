b9ee9dde6f6bd36a55161062e7058c7e1803fd60
samitha senewirathna <samithani17@gmail.com>
2025-07-07 14:39:39 +0530

    implement Security

---
+++ E-commerce-back-End/ecommerce-gateway/src/main/java/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) throws Exception {
+        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec ->
+                        authorizeExchangeSpec.pathMatchers("/").permitAll()
+                                .anyExchange()
+                                .authenticated())
+                                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+                        return serverHttpSecurity.build();
+    }
+}
